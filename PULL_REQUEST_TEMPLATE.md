9d7b990b40b579eb46f70ec653352a8cee860336
Adrian Opris <adrian.opris@customsoft.ro>
2021-12-15 17:00:26 +0200

    Add i18n on order quote link label and and reset PULL_REQUEST_TEMPLATE.md changes

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,14 +1,6 @@
 #### What does this PR do? \*
-Add order quote link to My account menu
-#### How to test it? \*
-##### 1. Add on manifest.json -> peerDependencies
 
-      "vtex.orderquote": "1.x"
-##### 2. Add these blocks on blocks.jsonc
-   
-    "oq-link": {}, 
-  
-    "my-account-page.order-quote-page": {} 
+#### How to test it? \*
   
 #### Describe alternatives you've considered, if any. \*
 
--- react/OrderQuoteLink.tsx
+++ react/OrderQuoteLink.tsx
@@ -1,10 +1,18 @@
 import { FunctionComponent, ReactElement } from 'react'
+import { defineMessages, useIntl } from 'react-intl'
+
+const messages = defineMessages({
+  orderQuoteLinkText: {
+    id: "store/orderquote.linkName",
+  },
+})
 
 const OrderQuoteLink: FunctionComponent<Props> = ({ render }) => {
+  const { formatMessage } = useIntl()
 
   return render([
     {
-      name: 'Order Quote',
+      name: formatMessage(messages.orderQuoteLinkText),
       path: `/order-quote`,
     },
   ])
